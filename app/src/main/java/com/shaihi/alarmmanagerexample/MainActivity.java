04f4a701e583ad708eb8c0f53b84bf4f679927e5
shaihi <shaihi@icloud.com>
2024-11-10 13:33:27 +0200

    Declared and initialized AlarmManager and PendingIntent

--- app/src/main/java/com/shaihi/alarmmanagerexample/MainActivity.java
+++ app/src/main/java/com/shaihi/alarmmanagerexample/MainActivity.java
@@ -1,6 +1,11 @@
 package com.shaihi.alarmmanagerexample;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +14,10 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView timerText;
+    private Button startButton;
+    private AlarmManager alarmManager;
+    private PendingIntent pendingIntent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +29,11 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        timerText = findViewById(R.id.timerText);
+        startButton = findViewById(R.id.startButton);
+        alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+        pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_IMMUTABLE);
     }
 }
